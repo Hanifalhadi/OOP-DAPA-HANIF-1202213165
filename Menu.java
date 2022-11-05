5a4357214a34976633d324968907a5fb58b2f96f
Hanifalhadi <115731681+Hanifalhadi@users.noreply.github.com>
2022-11-05 13:47:57 +0700

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,26 @@
+import java.util.jar.Attributes.Name;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Menu {  
+    String name;
+    String category;
+    int price;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCategory(String category){
+        this.category = category;
+    }
+    public void setPrice(Integer price){
+        this.price = price;
+    }
+
+}
+
+
+ 
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+
